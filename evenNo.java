66cf48a6f1121b26a421253de8e6787b2ba22e20
dac <dac@students.acts.in>
2024-12-22 08:29:37 +0530

    First commit

---
+++ evenNo.java
@@ -0,0 +1,9 @@
+public class evenNo {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 2; i <= 200; i += 2) {
+            sum += i;
+        }
+        System.out.println("Sum of first 100 even numbers: " + sum);
+    }
+}
