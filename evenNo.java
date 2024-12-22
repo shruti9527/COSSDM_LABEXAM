0cca3bc89baee2c744f2c49b8d01ef6069abfabd
shruti9527 <deshpandeshrutiy@gmail.com>
2024-12-22 08:46:13 +0530

    First commit

--- evenNo.java
+++ evenNo.java
@@ -7,3 +7,13 @@ public class evenNo {
         System.out.println("Sum of first 100 even numbers: " + sum);
     }
 }
+
+
+//git config --get user.name
+//git config --get user.email
+git config --get user.name
+git config --global user.name "YourCorrectUsername"
+git config --global user.email "your-email@example.com"
+git remote -v
+git remote set-url origin https://<your_username>:<your_token>@github.com/shruti9527/COSSDM_LABEXAM.git
+git credential-manager erase
